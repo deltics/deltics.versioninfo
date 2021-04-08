13b8648206b7c1129ded7405fb6c21061ac32f38
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-04-08 20:14:24 +1200

    feat: Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-deltics.versioninfo
+# Deltics.VersionInfo
+
---
+++ tests/test.dpr
@@ -0,0 +1,15 @@
+
+{$define CONSOLE}
+
+{$i deltics.smoketest.inc}
+
+  program test;
+
+uses
+  FastMM4,
+  Deltics.Smoketest,
+  Deltics.VersionInfo in '..\src\Deltics.VersionInfo.pas';
+
+begin
+
+end.
